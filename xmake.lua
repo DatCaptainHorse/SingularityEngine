36669a1253b77fb9870d1033e2b353e85c028b22
Kristian Ollikainen <krisu44@hotmail.com>
2022-10-02 22:55:55 +0300

    Project: Use releasedbg, default to faster optimization.
    Default to releases with debug information.
    O2 uses different options for linking, might fix Thread tests.

--- xmake.lua
+++ xmake.lua
@@ -1,4 +1,8 @@
-add_rules("mode.debug", "mode.release")
+add_rules("mode.releasedbg", "mode.debug")
+
+if is_mode("releasedbg") or is_mode("release") then
+    set_optimize("faster")
+end
 
 set_rules("c++")
 set_languages("c++20")
